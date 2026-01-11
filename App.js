910dbc50a10811f585da981384c17865693af842
Mac1618 <ma46099@gmail.com>
2026-01-11 15:51:19 +0800

    section 2: Core components and some attributes

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const handleTest = () => {
+		Alert.alert('testing the button');
+	};
+
+	return (
+		<View style={styles.container}>
+			<Text>Hello World!</Text>
+			<StatusBar style="auto" />
+
+			<View>
+				<Text>For testing</Text>
+				<Button onPress={handleTest} title="test confirmed" />
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
